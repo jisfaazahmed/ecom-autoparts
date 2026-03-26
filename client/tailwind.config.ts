b49b1563b0a7f8bd099d3e16db9f8490f9113537
RoshanMohamad <sarafroshan39@gmail.com>
2026-03-26 16:11:38 +0530

    feat: add container configuration to Tailwind CSS for improved layout control

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -7,6 +7,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
   	extend: {
   		colors: {
   			border: "hsl(var(--border))",
